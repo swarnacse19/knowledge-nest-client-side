50982c7cc27018326e46cb625a0269be74c72764
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-06-07 21:51:33 +0600

    added banner section

---
+++ src/components/Banner.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Banner() {
+  return (
+    <div
+      className="hero h-[520px]"
+      style={{
+        backgroundImage: "url('https://i.ibb.co/HL0qXW75/reading.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className=""></div>
+
+      <div className="hero-content text-center text-black">
+        <div className="max-w-xl">
+          <h1 className="mb-5 text-2xl md:text-5xl font-bold">Share Your Knowledge</h1>
+          <p className="mb-5 text-lg">
+            An interactive platform designed for students to share insightful articles,
+            exchange knowledge, and foster meaningful discussions through comments and collaboration.
+          </p>
+          <a href="/articles" className="btn px-12 bg-black text-white font-semibold">
+            Explore Articles
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
   );
 
   return (
-    <div className="navbar bg-base-100 px-8 py-2 shadow-md flex justify-between items-center">
+    <div className="navbar bg-base-100 px-12 py-4 shadow-md flex justify-between items-center">
       <div className="flex items-center gap-2">
         <GiSpellBook size={40} color='indigo'/>
         <h2 className="hidden md:flex text-2xl font-semibold text-purple-900">KnowledgeNest</h2>
