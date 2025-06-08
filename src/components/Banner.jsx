dc3266dab881d1d549fdcbd2e94ddc4bc4824ebc
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-06-08 20:36:50 +0600

    added animation color on banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
+import { motion } from "motion/react";
 
 function Banner() {
   return (
@@ -6,22 +8,37 @@ function Banner() {
       className="hero h-[520px]"
       style={{
         backgroundImage: "url('https://i.ibb.co/HL0qXW75/reading.jpg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
-      <div className=""></div>
-
       <div className="hero-content text-center text-black">
-        <div className="max-w-xl">
-          <h1 className="mb-5 text-2xl md:text-5xl font-bold">Share Your Knowledge</h1>
-          <p className="mb-5 text-lg">
-            An interactive platform designed for students to share insightful articles,
-            exchange knowledge, and foster meaningful discussions through comments and collaboration.
+        <div className="max-w-2xl">
+          <motion.h1
+            animate={{
+              color: ["#006400", "#800000", "#310062"],
+              transition: {
+                duration: 6,
+                repeat: Infinity,
+                repeatType: "loop",
+                ease: "easeInOut", 
+              },
+            }}
+            className="mb-5 text-2xl md:text-5xl font-bold"
+          >
+            Share Your Knowledge
+          </motion.h1>
+          <p className="mb-8 text-lg text-gray-700">
+            An interactive platform designed for students to share insightful
+            articles, exchange knowledge, and foster meaningful discussions
+            through comments and collaboration.
           </p>
-          <a href="/articles" className="btn px-12 bg-black text-white font-semibold">
+          <Link
+            to="/articles"
+            className="py-3 rounded-md px-12 bg-black text-white font-semibold"
+          >
             Explore Articles
-          </a>
+          </Link>
         </div>
       </div>
     </div>
