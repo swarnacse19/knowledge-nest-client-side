002ccd61518612a6128c7571e08df0f476d4460b
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-06-13 13:34:28 +0600

    show all cateories from db

---
+++ src/pages/Categories.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const loadCategories = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/categories");
+        const data = await res.json();
+        setCategories(data);
+      } catch (err) {
+        console.error("Failed to fetch categories:", err);
+      }
+    };
+
+    loadCategories();
+  }, []);
+
+  return (
+    <div className="my-10 max-w-11/12 mx-auto">
+      <h2 className="text-2xl font-bold mb-5 text-center">Categories</h2>
+      <div className="flex flex-wrap gap-3 justify-center">
+        {categories.map((cat, index) => (
+          <Link
+            to={`/category/${cat}`}
+            key={index}
+            className="px-4 py-2 bg-gray-200 rounded-full hover:bg-gray-300 transition"
+          >
+            {cat}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import FeaturedArticles from './FeaturedArticles';
+import Categories from './Categories';
 
 function Home() {
   return (
     <div>
       <Banner></Banner>
       <FeaturedArticles></FeaturedArticles>
+      <Categories></Categories>
     </div>
   );
 }
